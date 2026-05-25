073c7b4f8a8aaec0e85558786faad7911dd43c72
toanho <minhtoanho@gmail.com>
2026-05-25 15:02:59 +0700

    update zshrc config

--- oh-my-zsh-custom/custom/zshrc.zsh
+++ oh-my-zsh-custom/custom/zshrc.zsh
@@ -184,7 +184,7 @@ if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
     zle -N zle-line-finish
 fi
 
-export NVM_DIR="$HOME/.config/nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
